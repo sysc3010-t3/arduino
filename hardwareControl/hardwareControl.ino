2d31406efb70f4318ef3c0de035e99de5f91096a
Thao-Tran Le-Phuong <thaotran.lp@gmail.com>
2019-11-15 23:35:25 -0500

    Implemented serial commands * Motor control and LEDs can be controlled via serial commands * Fixed motor control functionality

--- hardwareControl/hardwareControl.ino
+++ hardwareControl/hardwareControl.ino
@@ -1,8 +1,10 @@
 // wired connections
-#define HG7881_A_IA 9 // D6 --> Motor A Input A --> MOTOR B +
-#define HG7881_A_IB 8 // D7 --> Motor A Input B --> MOTOR B -
-#define HG7881_B_IA 7 // D6 --> Motor B Input A --> MOTOR B +
-#define HG7881_B_IB 6 // D7 --> Motor B Input B --> MOTOR B -
+#define HG7881_A_IA 11 // D6 --> Motor A Input A --> MOTOR B +
+#define HG7881_A_IB 12 // D7 --> Motor A Input B --> MOTOR B -
+#define HG7881_B_IA 6 // D6 --> Motor B Input A --> MOTOR B +
+#define HG7881_B_IB 7 // D7 --> Motor B Input B --> MOTOR B -
+#define LEFT_LED 2
+#define RIGHT_LED 4
  
 // functional connections
 #define MOTOR_A_PWM HG7881_A_IA // Motor A PWM Speed
@@ -10,8 +12,11 @@
 #define MOTOR_B_PWM HG7881_B_IA // Motor B PWM Speed
 #define MOTOR_B_DIR HG7881_B_IB // Motor B Direction
 
+#define INPUT_MAX 256
+
 int motorSpeedA = 0;
 int motorSpeedB = 0;
+bool fwd = true;
 
 void setup() {
   pinMode(MOTOR_A_PWM, OUTPUT);
@@ -23,35 +28,60 @@ void setup() {
 }
 
 void loop() {
-  while (Serial.available() <= 0) {}
-  // read the incoming byte:
-  int xAxis = Serial.readStringUntil('\n').toInt();
-  Serial.print("x-axis: ");
-  Serial.println(xAxis);
-  Serial.flush();
-  while (Serial.available() <= 0) {}
-  int yAxis = Serial.readStringUntil('\n').toInt();
-  Serial.print("y-axis: ");
-  Serial.println(yAxis);
-  
+  if (Serial.available() > 0) {
+    char input[INPUT_MAX];
+    byte size = Serial.readBytesUntil('\n', input, INPUT_MAX);
+    input[size] = 0;
+
+    char tmp[size+1];
+    strcpy(tmp, input);
+    Serial.println(tmp);
+
+    // Define variables to store inputs
+    char msgType[8] = {0};
+    int values[2] = {0};
+    int i = 0; // current index of `values` array
+    
+    // Parse input
+    char* strtokIdx = strtok(input, ":");
+    while (strtokIdx != NULL) {
+      char key[8] = {0};
+      strcpy(key, strtokIdx);
+      
+      strtokIdx = strtok(NULL, ",");
+      if (strtokIdx == NULL) {
+        break;
+      }
+
+      char value[8] = {0};
+      strcpy(value, strtokIdx);
+
+      if (!strcmp(key, "type")) {
+        strcpy(msgType, value);
+      } else {
+        values[i++] = atoi(value);
+      }
+      
+      strtokIdx = strtok(NULL, ":");
+    }
+
+    if (!strcmp(msgType, "move")) {
+      setMotorSpeed(values[0], values[1]);
+    } else if (!strcmp(msgType, "lights")) {
+      setLights(values[0]);
+    }
+  }
+}
+
+void setMotorSpeed(int xAxis, int yAxis) {
   // Y-axis used for forward and backward control
   if (yAxis < 470) {
-    // Set Motor A backward
-    digitalWrite(MOTOR_A_PWM, HIGH);
-    digitalWrite(MOTOR_A_DIR, LOW);
-    // Set Motor B backward
-    digitalWrite(MOTOR_B_PWM, HIGH);
-    digitalWrite(MOTOR_B_DIR, LOW);
+    fwd = false;
     // Convert the declining Y-axis readings for going backward from 470 to 0 into 0 to 255 value for the PWM signal for increasing the motor speed
     motorSpeedA = map(yAxis, 470, 0, 0, 255);
     motorSpeedB = map(yAxis, 470, 0, 0, 255);
   } else if (yAxis > 550) {
-    // Set Motor A backward
-    digitalWrite(MOTOR_A_PWM, LOW);
-    digitalWrite(MOTOR_A_DIR, HIGH);
-    // Set Motor B forward
-    digitalWrite(MOTOR_B_PWM, LOW);
-    digitalWrite(MOTOR_B_DIR, HIGH);
+    fwd = true;
     // Convert the increasing Y-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
     motorSpeedA = map(yAxis, 550, 1023, 0, 255);
     motorSpeedB = map(yAxis, 550, 1023, 0, 255);
@@ -61,6 +91,7 @@ void loop() {
     motorSpeedA = 0;
     motorSpeedB = 0;
   }
+  
   // X-axis used for left and right control
   if (xAxis < 470) {
     // Convert the declining X-axis readings from 470 to 0 into increasing 0 to 255 value
@@ -90,14 +121,52 @@ void loop() {
       motorSpeedB = 0;
     }
   }
+  
   // Prevent buzzing at low speeds (Adjust according to your motors. My motors couldn't start moving if PWM value was below value of 70)
-  if (motorSpeedA < 70) {
+  if (motorSpeedA < 120) {
     motorSpeedA = 0;
   }
-  if (motorSpeedB < 70) {
+  if (motorSpeedB < 120) {
     motorSpeedB = 0;
+  }
+
+  Serial.print("motorSpeedA: ");
+  Serial.println(motorSpeedA);
+  Serial.print("motorSpeedB: ");
+  Serial.println(motorSpeedB);
+
+  if (fwd) {
+    // Set Motor A forward
+    digitalWrite(MOTOR_A_PWM, HIGH);
+    digitalWrite(MOTOR_A_DIR, HIGH);
+    // Set Motor B forward
+    digitalWrite(MOTOR_B_PWM, HIGH);
+    digitalWrite(MOTOR_B_DIR, HIGH);
+
+    // Forward speed is inverted so speed increases from 255 to 0
+    motorSpeedA = 255 - motorSpeedA;
+    motorSpeedB = 255 - motorSpeedB;
+  } else {
+    // Set Motor A backward
+    digitalWrite(MOTOR_A_PWM, LOW);
+    digitalWrite(MOTOR_A_DIR, LOW);
+    // Set Motor B backward
+    digitalWrite(MOTOR_B_PWM, LOW);
+    digitalWrite(MOTOR_B_DIR, LOW);
   }
 
   analogWrite(MOTOR_A_PWM, motorSpeedA); // Send PWM signal to motor A
   analogWrite(MOTOR_B_PWM, motorSpeedB); // Send PWM signal to motor B
+}
+
+void setLights(int state) {
+  if (state) {
+  Serial.println("setting lights to high");
+    digitalWrite(HEADLIGHT_1, HIGH);
+    digitalWrite(HEADLIGHT_2, HIGH);
+  } else {
+  Serial.println("setting lights to low");
+    digitalWrite(HEADLIGHT_1, LOW);
+    digitalWrite(HEADLIGHT_2, LOW);
+  }
 }
